14291bbda3f116f6c2cd298cff0b2497c25773cd
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-06-16 18:38:53 +0200

    More cleanup

--- definitions/adsy-centos-7.0/cleanup.sh
+++ definitions/adsy-centos-7.0/cleanup.sh
@@ -1,6 +1,6 @@
-yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts kernel.x86_64p
+yum -y install plymouth grub
 yum -y clean all
-rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
 
 # Remove traces of mac address from network configuration
