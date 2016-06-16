b573b2e53ab73374e6b8bef30757c1fcafa7c382
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-06-16 21:49:12 +0200

    Progressive cleaning

--- definitions/adsy-centos-7.2/base.sh
+++ definitions/adsy-centos-7.2/base.sh
@@ -2,11 +2,21 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
+# Delete stuff to keep the image small at any time -> better shrink
+rpm -qa | grep firmware | xargs yum -y erase
+echo /usr/share/locale/* | tr ' ' '\n' | grep -E "/de_|/en_" | xargs rm -rf
+rm -rf /usr/share/doc/*
+
 yum -y update
 yum -y clean all
 yum -y install epel-release
 yum -y install gcc make gcc-c++ zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils bzip2
 yum -y clean all
+
+# Delete stuff to keep the image small at any time -> better shrink
+rpm -qa | grep firmware | xargs yum -y erase
+echo /usr/share/locale/* | tr ' ' '\n' | grep -E "/de_|/en_" | xargs rm -rf
+rm -rf /usr/share/doc/*
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
--- definitions/adsy-centos-7.2/cleanup.sh
+++ definitions/adsy-centos-7.2/cleanup.sh
@@ -1,5 +1,9 @@
-yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts kernel.x86_64p
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y erase kernel-devel-`uname -r`
+rpm -qa | grep firmware | xargs yum -y erase
+rpm -qa | grep kernel | grep -v `uname -r` | xargs yum -y erase
+echo /usr/share/locale/* | tr ' ' '\n' | grep -E "/de_|/en_" | xargs rm -rf
+rm -rf /usr/share/doc/*
 yum -y install plymouth grub
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
