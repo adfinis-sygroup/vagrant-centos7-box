964d7b52912cead6e151cf561b2bfd98ef8d3ab3
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-06-16 20:20:35 +0200

    Oh well, forgot to change all the veewees to vagrant

--- definitions/adsy-centos-7.0/virtualbox.sh
+++ definitions/adsy-centos-7.0/virtualbox.sh
@@ -1,9 +1,9 @@
 # Installing the virtualbox guest additions
 yum -y install kernel-devel-`uname -r`
-VBOX_VERSION=$(cat /home/veewee/.vbox_version)
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-mount -o loop /home/veewee/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/veewee/VBoxGuestAdditions_*.iso
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
 reboot
