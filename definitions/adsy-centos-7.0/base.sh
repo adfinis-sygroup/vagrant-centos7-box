1f7a896ec150ad416e268f9e18d7f90886f49a80
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-06-16 18:09:22 +0200

    Better way to install epel

--- definitions/adsy-centos-7.0/base.sh
+++ definitions/adsy-centos-7.0/base.sh
@@ -2,14 +2,8 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/beta/7/\$basearch
-enabled=1
-gpgcheck=0
-EOM
-
+yum -y update
+yum -y install epel-release
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils bzip2
 
 # Make ssh faster by not waiting on DNS
