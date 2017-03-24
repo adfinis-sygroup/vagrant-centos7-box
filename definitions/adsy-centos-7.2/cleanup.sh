ef3ed9d8e16cb282e0a8127373e9f0fc70eaa7ac
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2017-03-24 12:32:10 +0100

    quoting `uname -r`

--- definitions/adsy-centos-7.2/cleanup.sh
+++ definitions/adsy-centos-7.2/cleanup.sh
@@ -1,11 +1,11 @@
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y erase kernel-devel-`uname -r`
+yum -y erase "kernel-devel-$(uname -r)"
 rpm -qa | grep firmware | xargs yum -y erase
 rpm -qa | grep NetworkManager | xargs yum -y erase
 rpm -qa | grep avahi | xargs yum -y erase
 rpm -qa | grep wpa_supplicant | xargs yum -y erase
 rpm -qa | grep kernel-headers | xargs yum -y erase
-rpm -qa | grep kernel | grep -v `uname -r` | xargs yum -y erase
+rpm -qa | grep kernel | grep -v "$(uname -r)" | xargs yum -y erase
 echo /usr/share/locale/* | tr ' ' '\n' | grep -E "/de_|/en_" | xargs rm -rf
 rm -rf /usr/share/doc/*
 yum -y install plymouth grub
